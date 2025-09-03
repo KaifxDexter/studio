1454fb98d4e27ebcf3cff94977c10c0e1a8996fb
Mohd Kaif <KaifNabeel125@gmail.com>
2025-09-03 08:38:17 +0000

    ₹ sign is not correctly displayed on web

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"PT Sans"', 'sans-serif'],
+        body: ['"Noto Sans"', 'sans-serif'],
+        headline: ['"Noto Sans"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
