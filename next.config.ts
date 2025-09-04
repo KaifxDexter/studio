c3791869827cc2b5ced2a33f9e415895764e9d5d
Mohd Kaif <KaifNabeel125@gmail.com>
2025-09-04 08:33:09 +0000

    website not opening

--- next.config.ts
+++ next.config.ts
@@ -36,7 +36,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https,'
+        protocol: 'https',
         hostname: 'sdmntprwestus.oaiusercontent.com',
         port: '',
         pathname: '/**',
